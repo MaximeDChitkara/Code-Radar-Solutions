bbf0f0e00823202e8dea72c9bb5c31d0210d5dba
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-01-21 12:25:41 +0530

    Solution to Input and Print Two Strings

--- Input and Print Two Strings@@6788d5012db25efec946f940/code.c
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -3,7 +3,7 @@ int main() {
     char* jspfreremesnomsdevariablessonttroplongsjepense = malloc(sizeof(char)*1000);
     char* oui = malloc(sizeof(char)*1000);
     scanf("%s",&jspfreremesnomsdevariablessonttroplongsjepense);
-    scanf("%s",&oui);
+    scanf("%s",oui);
     printf("You entered: %s and %s", jspfreremesnomsdevariablessonttroplongsjepense,oui);
     return 0;
 }
