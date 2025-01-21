4cb96d4263d4295adf767b4eadc3be330fc0467b
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-01-21 12:45:37 +0530

    Solution to Print Name, Age, and Hobby

--- Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
+++ Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
@@ -3,9 +3,9 @@
 int main() {
     char* name = malloc(sizeof(char)*1000);
     int age;
-    char* hobby = malloc(sizeof(char)*100);
+    char* hobby = malloc(sizeof(char)*1000);
     scanf("%s",name);
-    scanf("%i",age);
+    scanf("%i",&age);
     scanf("%s",hobby);
     printf("Name: %s\nAge: %i\nHobby: %c",name,age,hobby);
     return 0;
