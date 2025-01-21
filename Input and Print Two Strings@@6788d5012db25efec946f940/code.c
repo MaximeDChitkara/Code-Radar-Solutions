ddb04ecc879f2720e708dd451c0daa6c93ea71f2
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-01-21 12:23:00 +0530

    Solution to Input and Print Two Strings

---
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char* jspfreremesnomsdevariablessonttroplongsjepense;
+    char* oui;
+    scanf("%s",&jspfreremesnomsdevariablessonttroplongsjepense);
+    scanf("%s",&oui);
+    char* esrdtfgyhujikojihugyftdtgyhujikoihugftyhujgfhujgfchjuhgf = jspfreremesnomsdevariablessonttroplongsjepense + oui;
+    printf("You entered: %c",esrdtfgyhujikojihugyftdtgyhujikoihugftyhujgfhujgfchjuhgf);
+    return 0;
+}
