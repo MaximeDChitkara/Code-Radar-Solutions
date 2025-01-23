9e950fab995a1eae0fc214547288ea188bb5f605
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-01-23 14:21:03 +0530

    Solution to Check Positive or Negative

--- Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
+++ Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
@@ -3,6 +3,6 @@
 int main() {
     int a;
     scanf("%i",&a);
-    a >= 0 ? printf("Positive") : printf("Negative");
+    a > 0 ? printf("Positive") : a == 0 ? printf("Zero") : printf("Negative");
     return 0;
 }
