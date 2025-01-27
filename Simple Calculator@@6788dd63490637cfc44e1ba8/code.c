30f7b1c78cf68a1d2321061020bb11cc28752e3b
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-01-27 09:38:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,8 +2,8 @@
 
 int main() {
     int a,b;
-    char c;
-    scanf("%i %i %c", &a, &b, &c);
+    char* c;
+    scanf("%i %i %c", &a, &b, c);
     printf("%i", c == '+' ? a+b : c == '-' ? a-b : c == '*' ? a*b : a/b);
     return 0;
 }
