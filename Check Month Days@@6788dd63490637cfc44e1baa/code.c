cd84f80ee7904a004b02271db85222e1ea59c8e0
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-01-27 09:45:42 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -2,7 +2,11 @@
 
 
 int main() {
-    while(1==1)
-    fork();
+    int a;
+    scanf("%i", &a);
+    printf("%s", a == 1 ? "January" : a == 2 ? "February" : a == 3 ? "March" : a == 4 
+    ? "April" : a == 5 ? "May" : a == 6 ? "June" : a == 7 ? "July" : a == 8 ?
+    "August" : a == 9 ? "September" : a == 10 ?
+     "October" : a == 11 ? "November" : "December");
     return 0;
 }
