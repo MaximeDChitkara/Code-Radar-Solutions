926e91e0d7fa299d8c4117a89331201d583ccd69
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-02-07 07:37:20 +0100

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Fonction de tri à bulles
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {  
         for (int j = 0; j < n - i - 1; j++) {  
@@ -14,7 +13,6 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-// Fonction d'affichage du tableau
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d", arr[i]);
@@ -28,11 +26,13 @@ void printArray(int arr[], int n) {
 int main() {
     int n;
     scanf("%d", &n);
+    if (n <= 0) {
+        return 1;
+    }
 
-    // Allocation dynamique du tableau
     int *arr = (int*) malloc(n * sizeof(int));
     if (arr == NULL) {
-        return 1; // Erreur d'allocation mémoire
+        return 1;
     }
 
     for (int i = 0; i < n; i++) {
@@ -42,8 +42,6 @@ int main() {
     bubbleSort(arr, n);
     printArray(arr, n);
 
-    // Libération de la mémoire
     free(arr);
-
     return 0;
 }
