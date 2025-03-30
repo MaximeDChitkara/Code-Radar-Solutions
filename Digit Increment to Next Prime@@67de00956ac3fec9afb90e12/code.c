956019079028e84e9fc8dafd697c6e7f581e04f3
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-03-30 11:54:08 +0530

    Solution to Digit Increment to Next Prime

---
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -0,0 +1,21 @@
+int nextPrimeDigit(int n)
+{
+    if(n == 1 || 2) return 2;
+    if(n == 3) return 3;
+    if(n == 4 || 5) return 5;
+    if(n == 6 || 7) return 7;
+    if(n == 0 || 8 || 9) return 2;
+}
+int incrementToPrimeDigits(int n)
+[
+    int newNumber = 0;
+    int place = 1;
+    while(N > 0)
+    {
+        int digit = N % 10;
+        int newDigit = nextPrimeDigit(digit);
+        newNumber += newDigit * place;
+        N/=10; 
+    }
+    return newNumber;
+]
