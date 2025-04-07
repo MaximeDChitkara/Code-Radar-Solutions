dc0df6a0ab4b3e12996415c33bf3528dad9ef411
MaximeDChitkara <maxime1006.cse24@chitkara.edu.in>
2025-04-07 14:51:46 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,30 +1,23 @@
 void deflateBalloons(int air[], int n) {
-    int count = n;
-
     while (1) {
         int min = -1;
 
-        // Trouver la plus petite valeur positive
         for (int i = 0; i < n; i++) {
             if (air[i] > 0 && (min == -1 || air[i] < min)) {
                 min = air[i];
             }
         }
 
-        // Si toutes les ballons sont à plat
         if (min == -1) {
             break;
         }
 
-        // Compter combien de ballons sont encore gonflés
-        int inflated = 0;
+        printf("%d\n", n);
+
         for (int i = 0; i < n; i++) {
             if (air[i] > 0) {
-                inflated++;
                 air[i] -= min;
             }
         }
-
-        printf("%d\n", inflated);
     }
 }
